b43e1306386f6fbdf4049c8f38241ab5ca82522c
Ryo Akagi <ryoakg@gmail.com>
2016-08-05 07:12:11 +0900

    add DB module

---
+++ Framework/Db.php
@@ -0,0 +1,17 @@
+<?php
+namespace Framework;
+
+final class Db {
+    private function __construct() {}
+
+    static private $conn;
+
+    public static function getConnection(){
+        if (! self::$conn){
+            self::$conn = new \PDO('sqlite:data/test.db');
+            self::$conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            self::$conn->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+        }
+        return self::$conn;
+    }
+}
