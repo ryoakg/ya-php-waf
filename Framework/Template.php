319affc38918872fe3c27f72506ac9f7b1205950
Ryo Akagi <ryoakg@gmail.com>
2016-08-05 07:12:50 +0900

    add HTML template module

---
+++ Framework/Template.php
@@ -0,0 +1,25 @@
+<?php
+namespace Framework;
+
+final class Template {
+    static private function resolve($template){
+        return APP_ROOT_DIR . "/Template/{$template}.php";
+    }
+
+    static public function h($str){
+        return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
+    }
+
+    static public function render($template, $vars){
+        extract($vars);
+        require self::resolve($template);
+    }
+
+    static public function render_as_string($template, $vars){
+        extract($vars);
+        ob_start();
+        ob_implicit_flush(0);
+        require self::resolve($template);
+        return ob_get_clean();
+    }
+}
