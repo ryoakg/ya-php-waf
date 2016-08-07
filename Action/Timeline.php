1500ea8e579f21f9fce931dd9f416493fba4c650
Ryo Akagi <ryoakg@gmail.com>
2016-08-07 17:38:40 +0900

    tweets number limitation on timeline

--- Action/Timeline.php
+++ Action/Timeline.php
@@ -10,12 +10,15 @@ class Timeline {
             echo '404 Not Found.';
             exit;
         }
+        $page = filter_input(INPUT_GET, 'p', FILTER_VALIDATE_INT, ['options'=>['min_range'=>0]]);
+        if (! $page || $page < 0) $page = 0;
 
-        $tweets = array_map(function ($x){ return $x['body']; },
-                            \Model\Tweet::id_of($user_id));
-        $page = 0;
-        $n_pages = 10;
-        $page_range = 2;
+        $page_range = 2;        // 設定
+        $tweets_per_page = 5;   // 設定
+
+        $n_tweets = \Model\Tweet::n($user_id);
+        $n_pages = (int) ceil($n_tweets / $tweets_per_page);
+        $tweets = \Model\Tweet::id_of($user_id, $page*$tweets_per_page, $tweets_per_page);
         T::render('layout',
                   ['title' => 'タイムライン',
                    'logged_in' => \Model\Login::is_logged_in(),
@@ -27,7 +30,7 @@ class Timeline {
 
                                   'tweets' => $tweets,
 
-                                  'pagination' => ['url' => '',
+                                  'pagination' => ['url' => "/user/{$user_id}",
                                                    'n_pages' => $n_pages,
                                                    'current' => $page,
                                                    'range' => $page_range,
