e968df74f5f45e66c0257fb499e64230a5309c7e
Ryo Akagi <ryoakg@gmail.com>
2016-08-07 17:37:39 +0900

    add function n that returns number of tweets

--- Model/Tweet.php
+++ Model/Tweet.php
@@ -18,4 +18,12 @@ final class Tweet {
         $stmt->execute(array($user_id, $n, $from));
         return array_map(function ($x){ return $x['body']; }, $stmt->fetchAll());
     }
+
+    public static function n($user_id){
+        $db = \Framework\Db::getConnection();
+        $stmt = $db->prepare('SELECT count(*) AS n FROM tweet WHERE user_id = ?');
+        $stmt->execute(array($user_id));
+        $x = $stmt->fetch();
+        return (int)$x['n'];
+    }
 }
