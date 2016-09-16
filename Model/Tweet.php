8d63847c35ae3a10b2e43e0b1cb2b063210e352f
Ryo Akagi <ryoakg@gmail.com>
2016-09-16 13:47:58 +0900

    collecting tweets from a user and her follwees.

--- Model/Tweet.php
+++ Model/Tweet.php
@@ -3,6 +3,7 @@ namespace Model;
 
 final class Tweet {
     public static function create($user_id, $body){
+        // check wheather logged in
         $db = \Framework\Db::getConnection();
         $db->prepare('INSERT INTO tweet (user_id, body) values (?,?)')
             ->execute(array($user_id, $body));
@@ -17,6 +18,20 @@ final class Tweet {
                               OFFSET ?');
         $stmt->execute(array($user_id, $n, $from));
         return array_map(function ($x){ return $x['body']; }, $stmt->fetchAll());
+    }
+
+    public static function with_following_users($user_id, $from, $n){
+        $db = \Framework\Db::getConnection();
+        $stmt = $db->prepare('
+SELECT tweet.body, user.user_name, tweet.created_at
+  FROM tweet
+  JOIN user ON user_id = user.id
+ WHERE user_id = ? OR
+       user_id IN (SELECT followee_id FROM following WHERE follower_id = ?)
+ ORDER BY tweet.id DESC
+ LIMIT ? OFFSET ?');
+        $stmt->execute([$user_id, $user_id, $n, $from]);
+        return $stmt->fetchAll();
     }
 
     public static function n($user_id){
