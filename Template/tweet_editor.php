b44b692a32e11b56d9c997c32b14f78ad6531bcd
Ryo Akagi <ryoakg@gmail.com>
2016-09-17 10:16:09 +0900

    apply csrf token in Template/tweet_editor

--- Template/tweet_editor.php
+++ Template/tweet_editor.php
@@ -1,6 +1,6 @@
 <?php use \Framework\Template as T; ?>
 <form class="tweet-editor" method="POST" action="<?php echo $post_to; ?>">
-  <!-- <input type="hidden" name="token" value=""> -->
+  <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>">
   <textarea class="tweet-editor_content" name="tweet"></textarea>
   <button class="tweet-editor_submit-button" type="submit">Tweet!</button>
 </form>
