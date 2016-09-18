29a1d49ec9fec786f9fd962d356f25c97ed2cbff
Ryo Akagi <ryoakg@gmail.com>
2016-09-18 22:40:57 +0900

    validating parameter of Action/Tweet

--- Action/Tweet.php
+++ Action/Tweet.php
@@ -4,13 +4,13 @@ use \Framework\Template as T;
 
 class Tweet {
     public static function create(){
-        $user_id = $_SESSION['user_id'];
-        // ↓ 長さをチェック
-        $tweet = $_POST['tweet'];
-        // $_POST['token'];
-        if ($user_id && $tweet) {
-            \Model\Tweet::create($user_id, $tweet);
-        }
+        \Model\CSRF::ensure_token_is_valid(filter_input(INPUT_POST, 'csrf_token'));
+        if (! $user_id = \Model\Login::get_current_user_id()) throw new \Framework\UnauthorizedException();
+        if (! $tweet = filter_input(INPUT_POST, 'tweet')) throw new \Framework\InvalidRequestParameterException('tweet');
+
+        // ディスク容量が足りないとかでなければ例外は出ないはず
+        // 少なくともサーバ側の要因
+        \Model\Tweet::create($user_id, $tweet);
 
         $redirect_to = isset($_SESSION['prev_url']) ? $_SESSION['prev_url'] : "/user";
         header("Location: {$redirect_to}", true, 303);
