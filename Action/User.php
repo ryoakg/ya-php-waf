96a4d904b7091bb1aea50a00e0be1ec2a536ab19
Ryo Akagi <ryoakg@gmail.com>
2016-09-19 19:53:10 +0900

    simplify validation of Action/User

--- Action/User.php
+++ Action/User.php
@@ -3,6 +3,16 @@ namespace Action;
 use \Framework\Template as T;
 
 class User {
+    private static function get_username_from_post(){
+        return filter_input(INPUT_POST, 'user_name', FILTER_VALIDATE_REGEXP,
+                                 ['options'=>["regexp"=>"/[a-zA-Z0-9_\-]{4,20}/"]]);
+    }
+
+    private static function get_password_from_post(){
+        return filter_input(INPUT_POST, 'password', FILTER_VALIDATE_REGEXP,
+                            ['options'=>["regexp"=>"/[a-zA-Z0-9_!@#$%^&~`\\|:;<>{}\-]{8,20}/"]]);
+    }
+
     public static function signup_prompt(){
         T::render('layout',
                   ['title' => 'アカウント登録',
@@ -15,16 +25,16 @@ class User {
     }
 
     public static function signup(){
-        $name = filter_input(INPUT_POST, 'user_name', FILTER_VALIDATE_REGEXP,
-                             ['options'=>["regexp"=>"/[a-zA-Z0-9_\-]{4,20}/"]]);
-        if (! $name) throw new InvalidRequestParameterException('username');
-        $pass = filter_input(INPUT_POST, 'password', FILTER_VALIDATE_REGEXP,
-                             ['options'=>["regexp"=>"/[a-zA-Z0-9_!@#$%^&~`\\|:;<>{}\-]{8,20}/"]]);
-        if (! $password) throw new InvalidRequestParameterException('password');
+        if (! $name = self::get_username_from_post()) throw new \Framework\InvalidRequestParameterException('username');
+        if (! $password = self::get_password_from_post()) throw new \Framework\InvalidRequestParameterException('password');
 
         // ユーザ名が重複した場合は、例外が出るのでここで捕捉すべき
-        // APIなど作ってUI側もっていく事もできるけど、とりあえずやらない
-        \Model\User::create($name, $pass);
+        // APIなど作ってUI側もっていく事もできるけど、
+        // UI側で同じユーザ名はいないとチェックしたけど、
+        // サーバ側にリクエストが来た時点で、
+        // 他の誰かにユーザ名を取られて失敗する可能性はある
+        // とりあえず放っておく
+        \Model\User::create($name, $password);
 
         // https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.4
         //   the entity of the response SHOULD contain a short hypertext note with a hyperlink to the new URI(s).
@@ -44,27 +54,13 @@ class User {
     }
 
     public static function login(){
-        $err = [];
-        try{
-            $name = filter_input(INPUT_POST, 'user_name', FILTER_VALIDATE_REGEXP,
-                                 ['options'=>["regexp"=>"/[a-zA-Z0-9_\-]{4,20}/"]]);
-            $pass = filter_input(INPUT_POST, 'password', FILTER_VALIDATE_REGEXP,
-                                 ['options'=>["regexp"=>"/[a-zA-Z0-9_!@#$%^&~`\\|:;<>{}\-]{8,20}/"]]);
+        if (! $name = self::get_username_from_post()) throw new \Framework\InvalidRequestParameterException('username');
+        if (! $password = self::get_password_from_post()) throw new \Framework\InvalidRequestParameterException('password');
 
-            if ($name && $pass && \Model\Login::login($name, $pass)){
-                // https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.4
-                // 多分 303 が適切。あとでちゃんと読む
-
-                $redirect_to = "/user/{$_SESSION['user_id']}";
-                header("Location: {$redirect_to}", true, 303);
-            } else {
-                // DOS の対策でタイマで遅延した方がいい?
-                $_SESSION['FLASH'][] = 'ログイン失敗';
-                self::login_prompt();
-            }
-        } catch (\Exception $e){
-            header("Location: /hoge.php");
-        }
+        \Model\Login::login($name, $password);
+        $user_id = \Model\Login::get_current_user_id();
+        $redirect_to = "/user/{$user_id}";
+        header("Location: {$redirect_to}", true, 303);
     }
 
     public static function logout(){
