462300aa0ceb8ab823b0539268bc2fbd95666e29
Ryo Akagi <ryoakg@gmail.com>
2016-09-19 10:34:55 +0900

    form validation to input username and password

--- Template/user/input_id_and_password.php
+++ Template/user/input_id_and_password.php
@@ -4,13 +4,13 @@
     <tr>
       <th>ユーザID</th>
       <td>
-        <input type="text" name="user_name" value="<?php echo T::h($user_name); ?>" />
+        <input type="text" name="user_name" autofocus value="<?php echo T::h($user_name); ?>" required pattern="[a-zA-Z0-9_\-]{4,20}" title="半角英数とハイフンが使えます。4〜20文字で入力して下さい。" />
       </td>
     </tr>
     <tr>
       <th>パスワード</th>
       <td>
-        <input type="password" name="password" value="<?php echo T::h($password); ?>" />
+        <input type="password" name="password" value="<?php echo T::h($password); ?>" required pattern="[a-zA-Z0-9_!@#$%^&~`\\|:;<>{}\-]{8,20}" title="半角英数と !@#$%^&amp;~`\|:;&lt;&gt;{}- が使えます。8〜20文字で入力して下さい。" />
       </td>
     </tr>
   </tbody>
