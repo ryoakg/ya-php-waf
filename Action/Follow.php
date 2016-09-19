98f70761bbc152181819635ab4ccc9f8e09c59b2
Ryo Akagi <ryoakg@gmail.com>
2016-09-19 11:21:21 +0900

    abstract Action/Follow

--- Action/Follow.php
+++ Action/Follow.php
@@ -2,21 +2,21 @@
 namespace Action;
 
 class Follow {
-    public static function create($url_param){
+    private static function create_delete($fun, $url_param){
         $follower_id = $url_param['follower_id'];
         $followee_id = $url_param['followee_id'];
-        \Model\Following::follow((int)$follower_id, (int)$followee_id);
+
+        call_user_func($fun, (int)$follower_id, (int)$followee_id);
 
         $redirect_to = isset($_SESSION['prev_url']) ? $_SESSION['prev_url'] : "/user";
         header("Location: {$redirect_to}", true, 303);
     }
 
-    public static function delete($url_param){
-        $follower_id = $url_param['follower_id'];
-        $followee_id = $url_param['followee_id'];
-        \Model\Following::unfollow((int)$follower_id, (int)$followee_id);
+    public static function create($url_param){
+        self::create_delete(['\Model\Following','follow'], $url_param);
+    }
 
-        $redirect_to = isset($_SESSION['prev_url']) ? $_SESSION['prev_url'] : "/user";
-        header("Location: {$redirect_to}", true, 303);
+    public static function delete($url_param){
+        self::create_delete(['\Model\Following','unfollow'], $url_param);
     }
 }
