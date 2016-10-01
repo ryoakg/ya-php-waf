88c4e6edc4d41218eb7a459e99d855e551bef80a
Ryo Akagi <ryoakg@gmail.com>
2016-10-01 10:59:55 +0900

    add bin/web-app

--- examples/mini-blog/resources/router-script/router.php
+++ examples/mini-blog/resources/router-script/router.php
@@ -3,5 +3,5 @@ if (preg_match('#\A/(css|js|img)/#', $_SERVER["REQUEST_URI"])) {
     return false;               // ファイルシステムをそのまま反映
 }
 
-$_SERVER['SCRIPT_NAME'] = '/main.php';
+$_SERVER['SCRIPT_NAME'] = '/' . basename(__FILE__);
 require_once(__DIR__ . '/../..' . $_SERVER['SCRIPT_NAME']);
